b7e01c2b60569e2900f29166e4fc51f7cdce964e
younesh <younesharke@gmail.com>
2021-12-20 19:08:34 +0100

    d dist

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "dist" : "/",
+  /* publicPath:
+    process.env.NODE_ENV === "production"
+      ? "https://younesh.github.io/vuejs-tictactoe/dist/"
+      : "/", */
 };
