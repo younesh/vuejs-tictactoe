7f24ccb01c693439766ba5b643b97b7b52049243
younesh <younesharke@gmail.com>
2021-12-21 22:36:46 +0100

    refacto app home

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <GrilleMorpion />
     </div>
-    <router-view />
   </div>
 </template>
 
+<script>
+import GrilleMorpion from "@/components/GrilleMorpion.vue";
+export default {
+  components: { GrilleMorpion },
+};
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
